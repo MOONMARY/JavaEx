53935e51dbf3922e0e51a8e05f8eac691195945d
MOONHOJEONG <firstmoon5@naver.com>
2024-11-07 12:24:39 +0900

    Goods v3

--- src/com/javaex/oop/goods/v2/GoodsApp.java
+++ src/com/javaex/oop/goods/v2/GoodsApp.java
@@ -4,6 +4,7 @@ package com.javaex.oop.goods.v2;
 class Goods {
 	//필드선언 -> private
 	//정보는 은닉하고 Getter와 Setter를 통한 우회 접근이 필요
+	//데이터를 다루는 기능 (메서드) 함께 구현
 	private String name;
 	private int price;
 	
@@ -21,6 +22,13 @@ class Goods {
 	public void setPrice(int price) {
 		this.price = price;
 	}
+	
+	//일반 메서드
+	public void showInfo() {
+		System.out.println("상품명: " + name);
+		System.out.printf("가격: %,d원%n" + price);
+	}
+	
 }
 //Goods 테스트 클래스
 public class GoodsApp {
@@ -36,6 +44,7 @@ public class GoodsApp {
 		camera.setPrice(400_000);
 		
 //		System.out.printf("name : %s , price : %d", camera.name, camera.price);
-		System.out.printf("name : %s , price : %,d", camera.getName(), camera.getPrice());
+//		System.out.printf("name : %s , price : %,d", camera.getName(), camera.getPrice());
+		camera.showInfo();
 	}
 }
