4b1f31b77540e2c9e2de1fe6db41c3b5eb99cfa6
MOONHOJEONG <firstmoon5@naver.com>
2024-11-07 11:14:57 +0900

    getter/setter

--- src/com/javaex/oop/goods/v2/GoodsApp.java
+++ src/com/javaex/oop/goods/v2/GoodsApp.java
@@ -1,12 +1,27 @@
 package com.javaex.oop.goods.v2;
 
-//Goods V2. 필드
+//Goods V2. 접근제한자
 class Goods {
-	//필드선언
-	String name;
-	int price;
+	//필드선언 -> private
+	//정보는 은닉하고 Getter와 Setter를 통한 우회 접근이 필요
+	private String name;
+	private int price;
+	
+	//Getters	Setters
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+		//this : 현재 인스턴스 자체
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
 }
-
 //Goods 테스트 클래스
 public class GoodsApp {
 
@@ -14,12 +29,13 @@ public class GoodsApp {
 		// TODO Auto-generated method stub
 		Goods camera = new Goods();
 		//생성자를 호출 -> 인스턴스 생성 -> camera로 참조
-		camera.name = "LG그램";
-		camera.price = 9000_000;
-		System.out.printf("name : %s, price : %,d", camera.name, camera.price);
+//		camera.name = "Nikon";
+//		camera.price = 400_000;
 		
+		camera.setName("Nikon");
+		camera.setPrice(400_000);
 		
+//		System.out.printf("name : %s , price : %d", camera.name, camera.price);
+		System.out.printf("name : %s , price : %,d", camera.getName(), camera.getPrice());
 	}
-	
-
 }
