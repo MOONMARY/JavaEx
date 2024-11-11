46af0b2001b1d491d5fd78f8723b62aeb5224313
MOONHOJEONG <firstmoon5@naver.com>
2024-11-11 10:54:25 +0900

    null, array 예외

--- src/com/javaex/exception/ExecptionHandlingEx.java
+++ src/com/javaex/exception/ExecptionHandlingEx.java
@@ -7,7 +7,26 @@ public class ExecptionHandlingEx {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		arithExceptionEx();
+//		arithExceptionEx();
+		arrayExceptionEx();
+		nullPointerExcenptinoEx();
+	}
+	private static void arrayExceptionEx() {
+		int[] intArray = new int[] {3, 6, 9};
+		try {
+		System.out.println(intArray[3]);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.err.println("Error: " + e.getMessage());
+		}
+	}
+	private static void nullPointerExcenptinoEx() {
+		String str = new String("Hello Java");
+		try {
+		str = null;
+		System.out.println(str.toUpperCase());
+		} catch (NullPointerException e) {
+			System.err.println("null입니다");
+		}
 	}
 	
 	private static void arithExceptionEx() {
